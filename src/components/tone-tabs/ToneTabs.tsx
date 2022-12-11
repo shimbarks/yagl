97c2afff9020eeb123592b8d3fa593d092855ed8
Berkley, Shimi (sb2144) <sb2144@intl.att.com>
2022-12-12 00:59:58 +0200

    tabs styles

--- src/components/tone-tabs/ToneTabs.tsx
+++ src/components/tone-tabs/ToneTabs.tsx
@@ -1,6 +1,7 @@
 import * as Tabs from '@radix-ui/react-tabs';
 import { LetterTone, Yagl } from '../../models/app.model';
 import { yaglToCasual, yaglToFormal, yaglToFunny } from '../../utils/utils';
+import './ToneTabs.scss';
 
 export interface ToneTabsProps {
   yagl?: Yagl;
@@ -8,23 +9,25 @@ export interface ToneTabsProps {
 
 export const ToneTabs: React.FC<ToneTabsProps> = ({ yagl }) => {
   return (
-    <Tabs.Root defaultValue={LetterTone.CASUAL}>
+    <Tabs.Root className="tone-tabs" defaultValue={LetterTone.CASUAL}>
       <Tabs.List>
-        <Tabs.Trigger value={LetterTone.FORMAL}>
-          {LetterTone.FORMAL}
+        <Tabs.Trigger className="tab-button" value={LetterTone.FORMAL}>
+          {`Formal ${LetterTone.FORMAL}`}
         </Tabs.Trigger>
-        <Tabs.Trigger value={LetterTone.CASUAL}>
-          {LetterTone.CASUAL}
+        <Tabs.Trigger className="tab-button" value={LetterTone.CASUAL}>
+          {`Casual ${LetterTone.CASUAL}`}
         </Tabs.Trigger>
-        <Tabs.Trigger value={LetterTone.FUNNY}>{LetterTone.FUNNY}</Tabs.Trigger>
+        <Tabs.Trigger className="tab-button" value={LetterTone.FUNNY}>
+          {`Funny ${LetterTone.FUNNY}`}
+        </Tabs.Trigger>
       </Tabs.List>
-      <Tabs.Content value={LetterTone.FORMAL}>
+      <Tabs.Content className="tab-panel" value={LetterTone.FORMAL}>
         {yagl && yaglToFormal(yagl)}
       </Tabs.Content>
-      <Tabs.Content value={LetterTone.CASUAL}>
+      <Tabs.Content className="tab-panel" value={LetterTone.CASUAL}>
         {yagl && yaglToCasual(yagl)}
       </Tabs.Content>
-      <Tabs.Content value={LetterTone.FUNNY}>
+      <Tabs.Content className="tab-panel" value={LetterTone.FUNNY}>
         {yagl && yaglToFunny(yagl)}
       </Tabs.Content>
     </Tabs.Root>
