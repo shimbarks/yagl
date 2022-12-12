f63935c4f6b83b9b543cd2614cd53d8fc6f8b2e8
Berkley, Shimi (sb2144) <sb2144@intl.att.com>
2022-12-12 14:01:28 +0200

    input grouping

--- src/components/yagl-form/YaglForm.tsx
+++ src/components/yagl-form/YaglForm.tsx
@@ -1,6 +1,7 @@
 import { FieldErrorsImpl, UseFormRegister } from 'react-hook-form';
 import { Yagl } from '../../models/app.model';
 import { FormField } from '../form-field/FormField';
+import './YaglForm.scss';
 
 export interface YaglFormProps {
   register: UseFormRegister<Yagl>;
@@ -10,69 +11,75 @@ export interface YaglFormProps {
 export const YaglForm: React.FC<YaglFormProps> = ({ register, errors }) => {
   return (
     <>
-      <FormField
-        {...register('firstName')}
-        error={errors.firstName}
-        label="first name"
-        aria-required
-      />
-      <FormField
-        {...register('lastName')}
-        error={errors.lastName}
-        label="last name"
-        aria-required
-      />
-      <FormField
-        {...register('phone')}
-        error={errors.phone}
-        label="phone number"
-        type="tel"
-        aria-required
-      />
-      <FormField
-        {...register('email')}
-        error={errors.email}
-        type="email"
-        aria-required
-      />
-      <FormField
-        {...register('linkedIn')}
-        error={errors.linkedIn}
-        label="LinkedIn (optional)"
-        type="url"
-      />
-      <FormField
-        {...register('company')}
-        error={errors.company}
-        aria-required
-      />
-      <FormField
-        {...register('roles')}
-        error={errors.roles}
-        label="roles at the company (optional)"
-        pleaceholder="e.g. UI/UX designer, developer, team lead"
-      />
-      <FormField
-        {...register('startDate')}
-        error={errors.startDate}
-        type="date"
-        label="working start date"
-        aria-required
-      />
-      <FormField
-        {...register('endDate')}
-        error={errors.endDate}
-        type="date"
-        label="working end date"
-        aria-required
-      />
-      <FormField
-        {...register('lastDay')}
-        error={errors.lastDay}
-        type="date"
-        label="last day at office"
-        aria-required
-      />
+      <fieldset>
+        <legend>personal info</legend>
+        <FormField
+          {...register('firstName')}
+          error={errors.firstName}
+          label="first name"
+          aria-required
+        />
+        <FormField
+          {...register('lastName')}
+          error={errors.lastName}
+          label="last name"
+          aria-required
+        />
+        <FormField
+          {...register('phone')}
+          error={errors.phone}
+          label="phone number"
+          type="tel"
+          aria-required
+        />
+        <FormField
+          {...register('email')}
+          error={errors.email}
+          type="email"
+          aria-required
+        />
+        <FormField
+          {...register('linkedIn')}
+          error={errors.linkedIn}
+          label="LinkedIn (optional)"
+          type="url"
+        />
+      </fieldset>
+      <fieldset>
+        <legend>work history</legend>
+        <FormField
+          {...register('company')}
+          error={errors.company}
+          aria-required
+        />
+        <FormField
+          {...register('roles')}
+          error={errors.roles}
+          label="roles at the company (optional)"
+          pleaceholder="e.g. UI/UX designer, developer, team lead"
+        />
+        <FormField
+          {...register('startDate')}
+          error={errors.startDate}
+          type="date"
+          label="working start date"
+          aria-required
+        />
+        <FormField
+          {...register('endDate')}
+          error={errors.endDate}
+          type="date"
+          label="working end date"
+          aria-required
+        />
+        <FormField
+          {...register('lastDay')}
+          error={errors.lastDay}
+          type="date"
+          label="last day at office"
+          aria-required
+        />
+      </fieldset>
     </>
   );
 };
